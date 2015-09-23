fd1d71e6780d5431a2cb9eca00e7b3ae3b7743c0
Matt Loring <mattloring@google.com>
2015-09-23 15:04:23 -0700

    Push tags created by npm version in bump script

--- bin/run-bump.sh
+++ bin/run-bump.sh
@@ -5,5 +5,5 @@ git checkout master || exit 1
 git fetch -v "$2" || exit 1
 git rebase "$2/master" || exit 1
 npm version "$1" || exit 1
-git push "$2" master || exit 1
+git push "$2" master --tags || exit 1
 npm publish
