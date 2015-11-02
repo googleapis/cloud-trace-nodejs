3ffc2cd6cd574e906d4f15c75648fd364c749f64
Matt Loring <mattloring@google.com>
2015-11-02 11:57:48 -0800

    Run tests once on Travis
    Also use the new Travis container infrastructure.
    
    Fixes #135.

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,18 +1,58 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Usage: -c to report coverage
+
+while true; do
+  case $1 in
+    -c)
+      cover=1
+      ;;
+
+    *)
+      break
+  esac
+
+  shift
+done
+
+# Lint
 jshint . || exit 1
+
+# Install framework deps
 for dir in test/hooks/fixtures/*/ ;
 do
   echo -en "travis_fold:start:npm_install_${dir}\\r" | tr / _
   echo "npm install in ${dir}"
-  cd ${dir} && npm install && cd -
+  (cd "${dir}"; npm install) || exit 1
   echo -en "travis_fold:end:npm_install_${dir}\\r" | tr / _
 done
-istanbul test $(npm bin)/_mocha -- test test/hooks --timeout 4000 --R spec || exit 1
+
+# Get test/coverage command
+counter=0
+function run {
+  C="$(npm bin)/istanbul test"
+  if [ "$cover" ]; then
+    C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
+    ((counter++))
+  fi
+  ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
+}
+
+# Run test/coverage
+run test test/hooks
 for test in test/standalone/test-*.js ;
 do
-  istanbul test $(npm bin)/_mocha -- ${test} --timeout 4000 --R spec || exit 1
+  run "${test}"
 done
+
+# Conditionally publish coverage
+if [ "$cover" ]; then
+  istanbul report lcovonly
+  ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
+  rm -rf ./coverage
+fi
+
+# Run non-interference tests
 node test/non-interference/http-e2e.js || exit 1
 node test/non-interference/express-e2e.js || exit 1
 node test/non-interference/restify-e2e.js || exit 1
