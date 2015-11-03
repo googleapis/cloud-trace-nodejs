298f20b9aa03ba7fc874099ed9489637301a0af5
Matt Loring <mattloring@google.com>
2015-11-03 11:05:11 -0800

    No more warnings when root spans are sampled
    Mongo and redis no longer warn that tracing happened outside of a
    supported framework when root spans are sampled.
    
    Fixes #137.

---
+++ test/standalone/test-hooks-sample-warning.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// Prereqs:
+// Start docker daemon
+//   ex) docker -d
+// Run a mongo image binding the mongo port
+//   ex) docker run -p 27017:27017 -d mongo
+var agent = require('../..').start({ samplingRate: 1 }).private_();
+
+var common = require('../hooks/common.js');
+
+var assert = require('assert');
+var express = require('../hooks/fixtures/express4');
+var http = require('http');
+var mongoose = require('../hooks/fixtures/mongoose4');
+var redis = require('../hooks/fixtures/redis2');
+var warnCount = 0;
+var oldWarn = agent.logger.warn;
+var newWarn = function(error) {
+  if (error.indexOf('redis') !== -1 || error.indexOf('mongo') !== -1 ||
+      error.indexOf('http') !== -1) {
+    warnCount++;
+  }
+};
+
+describe('express + dbs', function() {
+  beforeEach(function() {
+    agent.logger.warn = newWarn;
+  });
+
+  afterEach(function() {
+    agent.logger.warn = oldWarn;
+    warnCount = 0;
+  });
+
+  it('mongo should not warn', function(done) {
+    var app = express();
+    app.get('/', function (req, res) {
+      mongoose.connect('mongodb://localhost:27017/testdb', function(err) {
+        assert(!err, 'Skipping: no mongo server found at localhost:27017.');
+        mongoose.connection.close(function(err) {
+          assert(!err);
+          res.sendStatus(200);
+        });
+      });
+    });
+    var server = app.listen(common.serverPort, function() {
+      http.get({port: common.serverPort}, function(res) {
+        http.get({port: common.serverPort}, function(res) {
+          server.close();
+          common.cleanTraces();
+          assert.equal(warnCount, 2);
+          done();
+        });
+      });
+    });
+  });
+
+  it('redis should not warn', function(done) {
+    var app = express();
+    app.get('/', function (req, res) {
+      var client = redis.createClient();
+      client.quit(function() {
+        res.sendStatus(200);
+      });
+    });
+    var server = app.listen(common.serverPort + 1, function() {
+      http.get({port: common.serverPort + 1}, function(res) {
+        http.get({port: common.serverPort + 1}, function(res) {
+          server.close();
+          common.cleanTraces();
+          assert.equal(warnCount, 2);
+          done();
+        });
+      });
+    });
+  });
+
+  it('http should not warn', function(done) {
+    var app = express();
+    app.get('/', function (req, res) {
+      http.get('http://www.google.com/', function() {
+        res.sendStatus(200);
+      });
+    });
+    var server = app.listen(common.serverPort + 2, function() {
+      http.get({port: common.serverPort + 2}, function(res) {
+        http.get({port: common.serverPort + 2}, function(res) {
+          server.close();
+          common.cleanTraces();
+          assert.equal(warnCount, 2);
+          done();
+        });
+      });
+    });
+  });
+});
