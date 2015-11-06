96ad6be77b5044158bcf2d2a638a9488175fcfe6
Matt Loring <mattloring@google.com>
2015-11-06 09:54:44 -0800

    Fix mongodb crash if project num isn't found
    The crash is not reproducible for the other frameworks as those patches
    do not persist on long lived objects and thus execute after hooks have
    been disabled. This PR also fixes a race condition crash that could
    occur if hooks were disabled between the start and end of a single span
    (resulting in a null dereference of the agent).
    
    Fixes #149

---
+++ test/standalone/test-hooks-no-project-num.js
@@ -0,0 +1,139 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+delete process.env.GCLOUD_PROJECT_NUM;
+
+var assert = require('assert');
+
+describe('should not break without project num', function() {
+  it('mongo', function(done) {
+    var agent = require('../..').start();
+    var mongoose = require('../hooks/fixtures/mongoose4');
+    var Simple = mongoose.model('Simple', new mongoose.Schema({
+      f1: String,
+      f2: Boolean,
+      f3: Number
+    }));
+    mongoose.connect('mongodb://localhost:27017/testdb', function(err) {
+      assert(!err, 'Skipping: error connecting to mongo at localhost:27017.');
+      Simple.find({}, function(err, results) {
+        mongoose.connection.close(function(err) {
+          agent.stop();
+          done();
+        });
+      });
+    });
+  });
+
+  it('redis', function(done) {
+    var agent = require('../..').start();
+    var redis = require('../hooks/fixtures/redis2');
+    var client = redis.createClient();
+    client.set('i', 1, function() {
+      client.quit(function() {
+        agent.stop();
+        done();
+      });
+    });
+  });
+
+  it('express', function(done) {
+    var http = require('http');
+    var agent = require('../..').start();
+    var express = require('../hooks/fixtures/express4');
+    var app = express();
+    var server;
+    app.get('/', function (req, res) {
+      res.send('hi');
+      server.close();
+      agent.stop();
+      done();
+    });
+    server = app.listen(8081, function() {
+      http.get({ port: 8081 });
+    });
+  });
+
+  it('restify', function(done) {
+    var http = require('http');
+    var agent = require('../..').start();
+    var restify = require('../hooks/fixtures/restify3');
+    var server = restify.createServer();
+    server.get('/', function (req, res, next) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('hi');
+      res.end();
+      server.close();
+      agent.stop();
+      done();
+    });
+    server.listen(8081, function() {
+      http.get({ port: 8081 });
+    });
+  });
+
+  it('hapi', function(done) {
+    var http = require('http');
+    var agent = require('../..').start();
+    var hapi = require('../hooks/fixtures/hapi8');
+    var server = new hapi.Server();
+    server.connection({ port: 8081 });
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function(req, reply) {
+        reply('hi');
+        server.stop();
+        agent.stop();
+        done();
+      }
+    });
+    server.start(function() {
+      http.get({ port: 8081 });
+    });
+  });
+
+  it('http', function(done) {
+    var agent = require('../..').start();
+    var req = require('http').get({ port: 8081 });
+    req.on('error', function() {
+      agent.stop();
+      done();
+    });
+  });
+
+  it('mysql', function(done) {
+    var agent = require('../..').start();
+    var mysql = require('../hooks/fixtures/mysql2');
+    var pool = mysql.createPool({
+      host     : 'localhost',
+      user     : 'travis',
+      password : '',
+      database : 'test'
+    });
+    pool.getConnection(function(err, conn) {
+      assert(!err, 'Skipping: Failed to connect to mysql.');
+      conn.query('SHOW TABLES', function(err, result) {
+        conn.release();
+        agent.stop();
+        done();
+      });
+    });
+  });
+});
