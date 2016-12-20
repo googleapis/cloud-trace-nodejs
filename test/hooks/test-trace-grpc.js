085a38b7abee231bfa9ce956f7a8b0bea380a953
Dominic Kramer <DominicDKramer@google.com>
2016-12-20 15:05:27 -0800

    Restore prev unit tests to fix errors
    The code in commit c38b3ec1df9cf5f4906a604962bae050d86116c3
    that placed configuration options in the call to `startAgent()`
    caused unit tests to break because calling `startAgent()` with a
    new config on an already started agent ignores the new config.

--- test/hooks/test-trace-grpc.js
+++ test/hooks/test-trace-grpc.js
@@ -16,9 +16,8 @@
 'use strict';
 
 var common = require('./common.js');
-require('../..')().startAgent({
-  enhancedDatabaseReporting: true
-});
+var trace = require('../..')();
+trace.startAgent().private_().config_.enhancedDatabaseReporting = true;
 var assert = require('assert');
 var traceLabels = require('../../src/trace-labels.js');
 
