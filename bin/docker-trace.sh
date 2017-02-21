9fa8af30b70abf4fd79ccea29ab34e3466a13199
Kelvin Jin <kelvinjin@google.com>
2017-02-21 14:20:56 -0800

    Use docker container names and check args

--- bin/docker-trace.sh
+++ bin/docker-trace.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-COMMAND=$1
-if [ $COMMAND = 'start' ]; then
-  docker run -p 127.0.0.1:27017:27017 -d mongo
-  docker run -p 127.0.0.1:6379:6379 -d redis
-  docker run -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD='Password12!' -e MYSQL_DATABASE=test -d mysql
+if [ ! -z $1 ]; then
+  COMMAND=$1
+  if [ $COMMAND = 'start' ]; then
+    docker run --name trace-test-mongo -p 127.0.0.1:27017:27017 -d mongo &&\
+    docker run --name trace-test-redis -p 127.0.0.1:6379:6379 -d redis &&\
+    docker run --name trace-test-mysql -p 127.0.0.1:3306:3306 -e MYSQL_ROOT_PASSWORD='Password12!' -e MYSQL_DATABASE=test -d mysql
+    exit $?
+  elif [ $COMMAND = 'stop' ]; then
+    docker stop trace-test-mongo
+    docker stop trace-test-redis
+    docker stop trace-test-mysql
+    exit $?
+  fi
 fi
-if [ $COMMAND = 'stop' ]; then
-  docker stop `docker ps | sed -En 's/([a-f0-9]+)[[:space:]]+(redis|mongo|mysql).*/\1/p'`
-fi
+
+echo 'Usage: docker-trace.sh [start|stop]'
+exit 1
