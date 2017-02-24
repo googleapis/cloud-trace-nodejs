23a626490a66bd17acf189f2588e1bfa30c4ef24
Kelvin Jin <kelvinjin@google.com>
2017-02-23 16:57:07 -0800

    fixing grpc-context test

--- test/test-grpc-context.js
+++ test/test-grpc-context.js
@@ -52,6 +52,7 @@ Object.keys(versions).forEach(function(version) {
   var agent;
   var express;
   var grpc;
+  var httpDebugPrinted;
   describe('express + ' + version, function() {
     before(function(done) {
       agent = require('..').start({ samplingRate: 0 });
@@ -60,7 +61,7 @@ Object.keys(versions).forEach(function(version) {
 
       common.replaceDebugLogger(agent, function(error, uri) {
         if (error.indexOf('http') !== -1) {
-          assert.notStrictEqual(uri.indexOf('localhost'), -1);
+          httpDebugPrinted = true;
         }
       });
 
@@ -145,12 +146,17 @@ Object.keys(versions).forEach(function(version) {
       });
     });
 
+    beforeEach(function() {
+      httpDebugPrinted = false;
+    });
+
     after(function() {
       grpcServer.forceShutdown();
       server.close();
     });
 
     afterEach(function() {
+      assert.ok(httpDebugPrinted);
       common.cleanTraces(agent);
     });
 
