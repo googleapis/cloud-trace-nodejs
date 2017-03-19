fa94f7e36a740771f29134f7ef95253bb652ee60
tinovyatkin <tino@vtkn.io>
2017-03-19 18:35:30 -0400

    Koa 2.x support

--- src/plugins/plugin-koa.js
+++ src/plugins/plugin-koa.js
@@ -18,8 +18,6 @@
 const shimmer = require('shimmer');
 var urlParse = require('url').parse;
 
-const SUPPORTED_VERSIONS = '1.x';
-
 function createUseWrap(api) {
   return function useWrap(use) {
     return function useTrace() {
@@ -88,12 +86,88 @@ function createMiddleware(api) {
   };
 }
 
+function createMiddleware2x(api) {
+  return (ctx, next) => {
+    const { req, res } = ctx;
+    const { end } = res;
+    const options = {
+      name: urlParse(req.url).pathname,
+      traceContext: this.req.headers[api.constants.TRACE_CONTEXT_HEADER_NAME],
+      skipFrames: 3
+    };
+    api.runInRootSpan(options, (root) => {
+      if (!root) {
+        return;
+      }
+
+      api.wrapEmitter(req);
+      api.wrapEmitter(res);
+
+      const url = (req.headers['X-Forwarded-Proto'] || 'http') +
+        '://' + req.headers.host + req.url;
+
+      // we use the path part of the url as the span name and add the full
+      // url as a label
+      // req.path would be more desirable but is not set at the time our middlewear runs.
+      root.addLabel(api.labels.HTTP_METHOD_LABEL_KEY, req.method);
+      root.addLabel(api.labels.HTTP_URL_LABEL_KEY, url);
+      root.addLabel(api.labels.HTTP_SOURCE_IP, req.connection.remoteAddress);
+
+      const context = root.getTraceContext();
+      if (context) {
+        res.setHeader(api.constants.TRACE_CONTEXT_HEADER_NAME, context);
+      }
+
+      // wrap end
+      res.end = (...args) => {
+        res.end = end;
+        const returned = res.end(...args);
+
+        if (req.route && req.route.path) {
+          root.addLabel(
+            'koa/request.route.path', req.route.path);
+        }
+        root.addLabel(
+            api.labels.HTTP_RESPONSE_CODE_LABEL_KEY, res.statusCode);
+        root.endSpan();
+
+        return returned;
+      };
+      api.wrap(next);
+    });
+
+    return next();
+  };
+}
+
+function createUseWrap2(api) {
+  return function useWrap(use) {
+    return function useTrace() {
+      if (!this._google_trace_patched) {
+        this._google_trace_patched = true;
+        this.use(createMiddleware2x(api));
+      }
+      return use.apply(this, arguments);
+    };
+  };
+}
+
 module.exports = [
   {
     file: '',
-    versions: SUPPORTED_VERSIONS,
+    versions: '1.x',
     patch: function(koa, api) {
       shimmer.wrap(koa.prototype, 'use', createUseWrap(api));
+    },
+    unpatch: function(koa) {
+      shimmer.unwrap(koa.prototype, 'use');
+    }
+  },
+    {
+    file: '',
+    versions: '2.x',
+    patch: function(koa, api) {
+      shimmer.wrap(koa.prototype, 'use', createUseWrap2(api));
     },
     unpatch: function(koa) {
       shimmer.unwrap(koa.prototype, 'use');
