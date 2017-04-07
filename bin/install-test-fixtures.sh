b60d142fb96ef3c3fc64bbe8140b6429675e3231
Dominic Kramer <DominicDKramer@google.com>
2017-04-07 13:40:36 -0700

    Clean noisy output of `install-test-fixtures.sh` (#461)
    PR-URL: https://github.com/GoogleCloudPlatform/cloud-trace-nodejs/pull/461

--- bin/install-test-fixtures.sh
+++ bin/install-test-fixtures.sh
@@ -4,13 +4,13 @@
 for dir in test/plugins/fixtures/*/ ;
 do
   echo -en "travis_fold:start:npm_install_${dir}\\r" | tr / _
-  pushd "${dir}"
+  pushd "${dir}" > /dev/null
   if [ -d 'node_modules' ] ; then
     echo "Skipping npm install in ${dir} since node_modules already exists"
   else
     echo "npm install in ${dir}"
     npm install || exit 1
   fi
-  popd
+  popd > /dev/null
   echo -en "travis_fold:end:npm_install_${dir}\\r" | tr / _
 done
