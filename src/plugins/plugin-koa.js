9dfde43b6b6bb5eeb21da717746ad05bddf774b1
Matthew Loring <matthewloring@users.noreply.github.com>
2017-04-13 09:57:16 -0700

    Koa 2.x support (#464)
    PR-URL: https://github.com/GoogleCloudPlatform/cloud-trace-nodejs/pull/464

--- src/plugins/plugin-koa.js
+++ src/plugins/plugin-koa.js
@@ -18,18 +18,54 @@
 const shimmer = require('shimmer');
 var urlParse = require('url').parse;
 
-const SUPPORTED_VERSIONS = '1.x';
-
-function createUseWrap(api) {
-  return function useWrap(use) {
-    return function useTrace() {
-      if (!this._google_trace_patched) {
-        this._google_trace_patched = true;
-        this.use(createMiddleware(api));
-      }
-      return use.apply(this, arguments);
-    };
+function startSpanForRequest(api, req, res, next) {
+  var originalEnd = res.end;
+  var options = {
+    name: urlParse(req.url).pathname,
+    url: req.url,
+    traceContext: req.headers[api.constants.TRACE_CONTEXT_HEADER_NAME],
+    skipFrames: 4
   };
+  api.runInRootSpan(options, function(root) {
+    if (!root) {
+      return;
+    }
+
+    api.wrapEmitter(req);
+    api.wrapEmitter(res);
+
+    const url = (req.headers['X-Forwarded-Proto'] || 'http') +
+      '://' + req.headers.host + req.url;
+
+    // we use the path part of the url as the span name and add the full
+    // url as a label
+    // req.path would be more desirable but is not set at the time our middlewear runs.
+    root.addLabel(api.labels.HTTP_METHOD_LABEL_KEY, req.method);
+    root.addLabel(api.labels.HTTP_URL_LABEL_KEY, url);
+    root.addLabel(api.labels.HTTP_SOURCE_IP, req.connection.remoteAddress);
+
+    var context = root.getTraceContext();
+    if (context) {
+      res.setHeader(api.constants.TRACE_CONTEXT_HEADER_NAME, context);
+    }
+
+    // wrap end
+    res.end = function(chunk, encoding) {
+      res.end = originalEnd;
+      const returned = res.end(chunk, encoding);
+
+      if (req.route && req.route.path) {
+        root.addLabel(
+          'koa/request.route.path', req.route.path);
+      }
+      root.addLabel(
+          api.labels.HTTP_RESPONSE_CODE_LABEL_KEY, res.statusCode);
+      root.endSpan();
+
+      return returned;
+    };
+    api.wrap(next);
+  });
 }
 
 function createMiddleware(api) {
@@ -37,64 +73,52 @@ function createMiddleware(api) {
     /* jshint validthis:true */
     const req = this.req;
     const res = this.res;
-    const originalEnd = res.end;
-    var options = {
-      name: urlParse(req.url).pathname,
-      url: req.url,
-      traceContext: this.req.headers[api.constants.TRACE_CONTEXT_HEADER_NAME],
-      skipFrames: 3
-    };
-    api.runInRootSpan(options, function(root) {
-      if (!root) {
-        return;
-      }
 
-      api.wrapEmitter(req);
-      api.wrapEmitter(res);
-
-      const url = (req.headers['X-Forwarded-Proto'] || 'http') +
-        '://' + req.headers.host + req.url;
-
-      // we use the path part of the url as the span name and add the full
-      // url as a label
-      // req.path would be more desirable but is not set at the time our middlewear runs.
-      root.addLabel(api.labels.HTTP_METHOD_LABEL_KEY, req.method);
-      root.addLabel(api.labels.HTTP_URL_LABEL_KEY, url);
-      root.addLabel(api.labels.HTTP_SOURCE_IP, req.connection.remoteAddress);
-
-      var context = root.getTraceContext();
-      if (context) {
-        res.setHeader(api.constants.TRACE_CONTEXT_HEADER_NAME, context);
-      }
-
-      // wrap end
-      res.end = function(chunk, encoding) {
-        res.end = originalEnd;
-        const returned = res.end(chunk, encoding);
-
-        if (req.route && req.route.path) {
-          root.addLabel(
-            'koa/request.route.path', req.route.path);
-        }
-        root.addLabel(
-            api.labels.HTTP_RESPONSE_CODE_LABEL_KEY, res.statusCode);
-        root.endSpan();
-
-        return returned;
-      };
-      api.wrap(next);
-    });
+    startSpanForRequest(api, req, res, next);
 
     yield next;
   };
 }
 
+function createMiddleware2x(api) {
+  return function middleware(ctx, next) {
+    const req = ctx.req;
+    const res = ctx.res;
+
+    startSpanForRequest(api, req, res, next);
+
+    return next();
+  };
+}
+
+function patchUse(koa, api, createMiddlewareFunction) {
+  shimmer.wrap(koa.prototype, 'use', function useWrap(use) {
+    return function useTrace() {
+      if (!this._google_trace_patched) {
+        this._google_trace_patched = true;
+        this.use(createMiddlewareFunction(api));
+      }
+      return use.apply(this, arguments);
+    };
+  });
+}
+
 module.exports = [
   {
     file: '',
-    versions: SUPPORTED_VERSIONS,
+    versions: '1.x',
     patch: function(koa, api) {
-      shimmer.wrap(koa.prototype, 'use', createUseWrap(api));
+      patchUse(koa, api, createMiddleware);
+    },
+    unpatch: function(koa) {
+      shimmer.unwrap(koa.prototype, 'use');
+    }
+  },
+    {
+    file: '',
+    versions: '2.x',
+    patch: function(koa, api) {
+      patchUse(koa, api, createMiddleware2x);
     },
     unpatch: function(koa) {
       shimmer.unwrap(koa.prototype, 'use');
