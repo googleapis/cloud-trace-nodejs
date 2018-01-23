86df086a271216b93835427c52cb06515d54dceb
Kelvin Jin <kelvinjin@google.com>
2018-01-22 17:12:01 -0800

    build: set different language level targets depending on node version

--- scripts/index.ts
+++ scripts/index.ts
@@ -9,6 +9,7 @@
 const [bin, script, ...steps] = process.argv;
 
 import { checkInstall } from './check-install';
+import { compile } from './compile';
 import { encryptCredentials, decryptCredentials } from './credentials';
 import { initTestFixtures } from './init-test-fixtures';
 import { reportCoverage } from './report-coverage';
@@ -37,79 +38,83 @@ async function run(steps: string[]) {
         ]
       );
       continue;
-    }
-
-    switch (step) {
-      case 'check-install':
-        await checkInstall();
-        break;
-      case 'encrypt-service-account-credentials':
-        const keyAndIV = await encryptCredentials(`node-team-test-${keyID}.json`);
-        console.log([
-          `key: ${keyAndIV.key}`,
-          `iv: ${keyAndIV.iv}`
-        ].join('\n'));
-        break;
-      case 'decrypt-service-account-credentials':
-        const {
-          TRACE_SYSTEM_TEST_ENCRYPTED_CREDENTIALS_KEY: key,
-          TRACE_SYSTEM_TEST_ENCRYPTED_CREDENTIALS_IV: iv,
-        } = process.env;
-      
-        if (!key || !iv) {
-          console.log('> Environment insufficient to decrypt service account credentials');
+    } else if (step.startsWith('compile-')) {
+      const [_literalCompile, languageLevel, strict] = step.split('-');
+      await compile({ strict: !!strict, languageLevel });
+      continue;
+    } else {
+      switch (step) {
+        case 'check-install':
+          await checkInstall();
           break;
-        }
+        case 'encrypt-service-account-credentials':
+          const keyAndIV = await encryptCredentials(`node-team-test-${keyID}.json`);
+          console.log([
+            `key: ${keyAndIV.key}`,
+            `iv: ${keyAndIV.iv}`
+          ].join('\n'));
+          break;
+        case 'decrypt-service-account-credentials':
+          const {
+            TRACE_SYSTEM_TEST_ENCRYPTED_CREDENTIALS_KEY: key,
+            TRACE_SYSTEM_TEST_ENCRYPTED_CREDENTIALS_IV: iv,
+          } = process.env;
+        
+          if (!key || !iv) {
+            console.log('> Environment insufficient to decrypt service account credentials');
+            break;
+          }
 
-        await decryptCredentials({ key, iv }, `node-team-test-${keyID}.json`);
-        break;
-      case 'init-test-fixtures':
-        await initTestFixtures();
-        break;
-      case 'run-unit-tests':
-        await runTests({
-          globs: [
-            `${BUILD_DIRECTORY}/test/test-*.js`,
-            `${BUILD_DIRECTORY}/test/plugins/test-*.js`
-          ],
-          rootDir: BUILD_DIRECTORY,
-          coverage: false,
-          timeout: 4000
-        });
-        break;
-      case 'run-unit-tests-with-coverage':
-        await runTests({
-          globs: [
-            `${BUILD_DIRECTORY}/test/test-*.js`,
-            `${BUILD_DIRECTORY}/test/plugins/test-*.js`
-          ],
-          rootDir: BUILD_DIRECTORY,
-          coverage: true,
-          timeout: 4000
-        });
-        break;
-      case 'run-system-tests':
-        if (process.env.CI_PULL_REQUEST && !(await existsP('node-team-test-d0b0be11c23d.json'))) {
-          console.log('> Not running system tests in PRs');
-        } else {
+          await decryptCredentials({ key, iv }, `node-team-test-${keyID}.json`);
+          break;
+        case 'init-test-fixtures':
+          await initTestFixtures();
+          break;
+        case 'run-unit-tests':
           await runTests({
             globs: [
-              `system-test/*.js`,
+              `${BUILD_DIRECTORY}/test/test-*.js`,
+              `${BUILD_DIRECTORY}/test/plugins/test-*.js`
             ],
-            rootDir: '.',
-            coverage: false
+            rootDir: BUILD_DIRECTORY,
+            coverage: false,
+            timeout: 4000
           });
-        }
-        break;
-      case 'report-coverage':
-        await reportCoverage();
-        break;
-      case 'test-non-interference':
-        await testNonInterference();
-        break;
-      default:
-        console.log(`> ${step}: not found`);
-        break;
+          break;
+        case 'run-unit-tests-with-coverage':
+          await runTests({
+            globs: [
+              `${BUILD_DIRECTORY}/test/test-*.js`,
+              `${BUILD_DIRECTORY}/test/plugins/test-*.js`
+            ],
+            rootDir: BUILD_DIRECTORY,
+            coverage: true,
+            timeout: 4000
+          });
+          break;
+        case 'run-system-tests':
+          if (process.env.CI_PULL_REQUEST && !(await existsP('node-team-test-d0b0be11c23d.json'))) {
+            console.log('> Not running system tests in PRs');
+          } else {
+            await runTests({
+              globs: [
+                `system-test/*.js`,
+              ],
+              rootDir: '.',
+              coverage: false
+            });
+          }
+          break;
+        case 'report-coverage':
+          await reportCoverage();
+          break;
+        case 'test-non-interference':
+          await testNonInterference();
+          break;
+        default:
+          console.log(`> ${step}: not found`);
+          break;
+      }
     }
   }
 }
