476b2ab40c13bb59985795f76ce3dc591d52c5f2
Kelvin Jin <kelvinjin@google.com>
2018-04-16 10:27:24 -0700

    fix: adjust async_hooks cls behavior

--- src/cls/async-hooks.ts
+++ src/cls/async-hooks.ts
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-// This file requires continuation-local-storage in the AsyncHooksCLS
-// constructor, rather than upon module load.
-import * as asyncHooksModule from 'async_hooks';
+// This file requires async_hooks in the AsyncHooksCLS constructor, rather than
+// upon module load.
+import * as asyncHooks from 'async_hooks';
 import {EventEmitter} from 'events';
 import * as shimmer from 'shimmer';
 
 import {CLS, Func} from './base';
 
-type AsyncHooksModule = typeof asyncHooksModule;
+type AsyncHooksModule = typeof asyncHooks;
 
 // A list of well-known EventEmitter methods that add event listeners.
 const EVENT_EMITTER_METHODS: Array<keyof EventEmitter> =
@@ -31,29 +31,63 @@ const EVENT_EMITTER_METHODS: Array<keyof EventEmitter> =
 const WRAPPED = Symbol('@google-cloud/trace-agent:AsyncHooksCLS:WRAPPED');
 
 type ContextWrapped<T> = T&{[WRAPPED]?: boolean};
+type Reference<T> = {
+  value: T
+};
 
 /**
  * An implementation of continuation-local storage on top of the async_hooks
  * module.
  */
 export class AsyncHooksCLS<Context extends {}> implements CLS<Context> {
-  private currentContext: {value: Context};
-  private contexts: {[id: number]: Context} = {};
-  private hook: asyncHooksModule.AsyncHook;
+  // instance-scope reference to avoid top-level require.
+  private ah: AsyncHooksModule;
+
+  /** A map of AsyncResource IDs to Context objects. */
+  private contexts: {[id: number]: Reference<Context>} = {};
+  /** The AsyncHook that proactively populates entries in this.contexts. */
+  private hook: asyncHooks.AsyncHook;
+  /** Whether this instance is enabled. */
   private enabled = false;
 
   constructor(private readonly defaultContext: Context) {
-    this.currentContext = {value: this.defaultContext};
-    this.hook = (require('async_hooks') as AsyncHooksModule).createHook({
+    // Store a reference to the async_hooks module, since we will need to query
+    // the current AsyncResource ID often.
+    this.ah = require('async_hooks') as AsyncHooksModule;
+
+    // Create the hook.
+    this.hook = this.ah.createHook({
       init: (id: number, type: string, triggerId: number, resource: {}) => {
-        this.contexts[id] = this.currentContext.value;
-      },
-      before: (id: number) => {
-        if (this.contexts[id]) {
-          this.currentContext.value = this.contexts[id];
+        // init is called when a new AsyncResource is created. We want code
+        // that runs within the scope of this new AsyncResource to see the same
+        // context as its "parent" AsyncResource. The criteria for the parent
+        // depends on the type of the AsyncResource.
+        if (type === 'PROMISE') {
+          // Opt not to use the trigger ID for Promises, as this causes context
+          // confusion in applications using async/await.
+          // Instead, use the ID of the AsyncResource in whose scope we are
+          // currently running.
+          this.contexts[id] = this.contexts[this.ah.executionAsyncId()];
+        } else {
+          // Use the trigger ID for any other type. In Node core, this is
+          // usually equal the ID of the AsyncResource in whose scope we are
+          // currently running (the "current" AsyncResource), or that of one
+          // of its ancestors, so the behavior is not expected to be different
+          // from using the ID of the current AsyncResource instead.
+          // A divergence is expected only to arise through the user
+          // AsyncResource API, because users of that API can specify their own
+          // trigger ID. In this case, we choose to respect the user's
+          // selection.
+          this.contexts[id] = this.contexts[triggerId];
         }
+        // Note that this function always assigns values in this.contexts to
+        // values under other keys, which may or may not be undefined. Consumers
+        // of the CLS API will get the sentinel (default) value if they query
+        // the current context when it is stored as undefined.
       },
       destroy: (id: number) => {
+        // destroy is called when the AsyncResource is no longer used, so also
+        // delete its entry in the map.
         delete this.contexts[id];
       }
     });
@@ -64,51 +98,75 @@ export class AsyncHooksCLS<Context extends {}> implements CLS<Context> {
   }
 
   enable(): void {
-    this.currentContext.value = this.defaultContext;
+    this.contexts = {};
     this.hook.enable();
     this.enabled = true;
   }
 
   disable(): void {
-    this.currentContext.value = this.defaultContext;
+    this.contexts = {};
     this.hook.disable();
     this.enabled = false;
   }
 
   getContext(): Context {
-    return this.currentContext.value;
+    // We don't store this.defaultContext directly in this.contexts.
+    // Getting undefined when looking up this.contexts means that it wasn't
+    // set, so return the default context.
+    const current = this.contexts[this.ah.executionAsyncId()];
+    return current ? current.value : this.defaultContext;
   }
 
   setContext(value: Context): void {
-    this.currentContext.value = value;
+    this.contexts[this.ah.executionAsyncId()].value = value;
   }
 
   runWithNewContext<T>(fn: Func<T>): T {
-    const oldContext = this.currentContext.value;
-    this.currentContext.value = this.defaultContext;
+    // Run fn() so that any AsyncResource objects that are created in
+    // fn will have the context set by this.setContext.
+    const id = this.ah.executionAsyncId();
+    const oldContext = this.contexts[id];
+    // Reset the current context. This prevents this.getContext from returning
+    // a stale value.
+    this.contexts[id] = {value: this.defaultContext};
     try {
       return fn();
     } finally {
-      this.currentContext.value = oldContext;
+      // Revert the current context to what it was before any calls to
+      // this.setContext from within fn.
+      this.contexts[id] = oldContext;
     }
   }
 
   bindWithCurrentContext<T>(fn: Func<T>): Func<T> {
-    if ((fn as ContextWrapped<Func<T>>)[WRAPPED] || !this.currentContext) {
+    const outerId = this.ah.executionAsyncId();
+    // Return if we have already wrapped the function, or there is no current
+    // context to bind.
+    if ((fn as ContextWrapped<Func<T>>)[WRAPPED] || !this.contexts[outerId]) {
       return fn;
     }
-    const current = this.currentContext;
-    const boundContext = this.currentContext.value;
+    const that = this;
+    // Capture the context of the current AsyncResource.
+    const boundContext = this.contexts[outerId];
+    // Wrap fn so that any AsyncResource objects that are created in fn will
+    // share context with that of the AsyncResource with the given ID.
     const contextWrapper: ContextWrapped<Func<T>> = function(this: {}) {
-      const oldContext = current.value;
-      current.value = boundContext;
+      const innerId = that.ah.executionAsyncId();
+      const oldContext = that.contexts[innerId];
+      // Restore the captured context.
+      that.contexts[innerId] = boundContext;
       try {
         return fn.apply(this, arguments) as T;
       } finally {
-        current.value = oldContext;
+        // Revert the current context to what it was before it was set to the
+        // captured context.
+        that.contexts[innerId] = oldContext;
       }
     };
+    // Prevent re-wrapping.
     contextWrapper[WRAPPED] = true;
+    // Explicitly inherit the original function's length, because it is
+    // otherwise zero-ed out.
     Object.defineProperty(contextWrapper, 'length', {
       enumerable: false,
       configurable: true,
@@ -118,11 +176,6 @@ export class AsyncHooksCLS<Context extends {}> implements CLS<Context> {
     return contextWrapper;
   }
 
-  // This function is not technically needed and all tests currently pass
-  // without it (after removing call sites). While it is not a complete
-  // solution, restoring correct context before running every request/response
-  // event handler reduces the number of situations in which userspace queuing
-  // will cause us to lose context.
   patchEmitterToPropagateContext(ee: EventEmitter): void {
     const that = this;
     EVENT_EMITTER_METHODS.forEach((method) => {
