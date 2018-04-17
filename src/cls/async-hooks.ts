353e6b92a18dd60dda982a156a4f93df37eada05
Kelvin Jin <kelvinjin@google.com>
2018-04-17 14:55:21 -0700

    fix: restore context when a function run with a given context throws

--- src/cls/async-hooks.ts
+++ src/cls/async-hooks.ts
@@ -86,10 +86,12 @@ export class AsyncHooksCLS<Context extends {}> implements CLS<Context> {
   runWithNewContext<T>(fn: Func<T>): T {
     const oldContext = this.currentContext.value;
     this.currentContext.value = this.defaultContext;
-    // TODO(kjin) Handle the case where fn throws. Context: PR #708
-    const res = fn();
-    this.currentContext.value = oldContext;
-    return res;
+    try {
+      const res = fn();
+      return res;
+    } finally {
+      this.currentContext.value = oldContext;
+    }
   }
 
   bindWithCurrentContext<T>(fn: Func<T>): Func<T> {
@@ -101,10 +103,12 @@ export class AsyncHooksCLS<Context extends {}> implements CLS<Context> {
     const contextWrapper: ContextWrapped<Func<T>> = function(this: {}) {
       const oldContext = current.value;
       current.value = boundContext;
-      // TODO(kjin) Handle the case where fn throws. Context: PR #708
-      const res = fn.apply(this, arguments) as T;
-      current.value = oldContext;
-      return res;
+      try {
+        const res = fn.apply(this, arguments) as T;
+        return res;
+      } finally {
+        current.value = oldContext;
+      }
     };
     contextWrapper[WRAPPED] = true;
     Object.defineProperty(contextWrapper, 'length', {
