b547dc57b5aa2c71ff5545c9c7fdf529de852603
Kelvin Jin <kelvinjin@google.com>
2018-05-10 15:20:02 -0700

    test: refactor test-span-data

--- test/test-span-data.ts
+++ test/test-span-data.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Google Inc. All Rights Reserved.
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,177 +14,174 @@
  * limitations under the License.
  */
 
-'use strict';
+import * as assert from 'assert';
 
-import './override-gcp-metadata';
-import { cls } from '../src/cls';
-import { Constants } from '../src/constants';
+import {Constants, SpanDataType} from '../src/constants';
+import {BaseSpanData, ChildSpanData, RootSpanData} from '../src/span-data';
+import {Trace} from '../src/trace';
 import {TraceLabels} from '../src/trace-labels';
-import { RootSpanData, ChildSpanData } from '../src/span-data';
-import { Trace } from '../src/trace';
-import { traceWriter } from '../src/trace-writer';
+import {traceWriter, TraceWriter, TraceWriterConfig} from '../src/trace-writer';
 
-var assert = require('assert');
-var common = require('./plugins/common'/*.js*/);
+import {TestLogger} from './logger';
+import * as traceAgentModule from './trace';
 
-function createRootSpanData(name, traceId?, parentId?, skipFrames?) {
-  return new RootSpanData(
-    { projectId: '', traceId: traceId, spans: [] },
-    name,
-    parentId,
-    skipFrames
-  );
-}
+describe('SpanData', () => {
+  class CaptureSpanTraceWriter extends TraceWriter {
+    writeSpan(trace: Trace) {
+      capturedTrace = trace;
+    }
+  }
+  let capturedTrace: Trace;
+  let trace: Trace;
 
-describe('SpanData', function() {
-  var agent;
-  before(function() {
-    agent = require('../..').start({
-      projectId: '0',
-      samplingRate: 0
-    });
+  before(() => {
+    traceAgentModule.setTraceWriter(CaptureSpanTraceWriter);
+    traceWriter.create(
+        {
+          onUncaughtException: 'ignore',
+          maximumLabelValueSize: 16,
+          stackTraceLimit: 5
+        } as TraceWriterConfig,
+        new TestLogger());
   });
 
-  it('has correct default values', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      assert.ok(spanData.trace);
-      assert.strictEqual(spanData.trace.traceId, 1);
-      assert.ok(spanData.span.spanId);
+  after(() => {
+    traceAgentModule.setTraceWriter(traceAgentModule.TestTraceWriter);
+  });
+
+  beforeEach(() => {
+    trace = {projectId: '0', traceId: 'trace-id', spans: []};
+  });
+
+  describe('BaseSpanData', () => {
+    // We use the ChildSpanData constructor, but what we are really testing
+    // is the API that is common between child and root spans -- which is
+    // BaseSpanData.
+    class CommonSpanData extends ChildSpanData {}
+
+    it('exposes a Trace field', () => {
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      assert.strictEqual(spanData.trace, trace);
+    });
+
+    it('creates a TraceSpan structure with expected fields', () => {
+      const spanData = new CommonSpanData(trace, 'name', '400', 0);
       assert.strictEqual(spanData.span.name, 'name');
+      assert.strictEqual(spanData.span.parentSpanId, '400');
+      assert.ok(spanData.span.spanId.match(/[0-9A-F]{12}/));
     });
-  });
 
-  it('generates unique numeric span ID strings', function() {
-    cls.get().runWithNewContext(function() {
-      var spans = [];
-      var spanIds = new Set<string>();
-      var rootSpanData = createRootSpanData('hi');
-      var numSpanIdsToCheck = 5;
-      for (var i = 0; i < numSpanIdsToCheck; i++) {
-        var spanData = new ChildSpanData(
-            rootSpanData.trace, 'child', rootSpanData.span.spanId, 0);
-        var spanId = spanData.span.spanId;
-        spanIds.add(spanId);
-        // Check that the span IDs are numeric positive number strings
-        assert.ok(typeof spanId === 'string');
-        assert.ok(spanId.match(/\d+/));
-        assert.ok(Number(spanId) > 0);
-        assert.strictEqual(Number(spanId).toString(), spanId);
+    it('creates spans with unique span IDs', () => {
+      const numSpans = 5;
+      const spanIds: Set<string> = new Set();
+      for (let i = 0; i < numSpans; i++) {
+        const spanData = new CommonSpanData(trace, 'name', '400', 0);
+        spanIds.add(spanData.span.spanId);
       }
+      assert.strictEqual(spanIds.size, numSpans);
     });
-  });
 
-  it('converts label values to strings', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      spanData.addLabel('a', 'b');
-      assert.strictEqual(spanData.span.labels.a, 'b');
-      spanData.addLabel('c', 5);
-      assert.strictEqual(spanData.span.labels.c, '5');
-    });
-  });
-
-  it('serializes object labels correctly', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      spanData.addLabel('a', [{i: 5}, {j: 6}]);
-      assert.strictEqual(spanData.span.labels.a, '[ { i: 5 }, { j: 6 } ]');
-    });
-  });
-
-  it('serializes symbol labels correctly', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      spanData.addLabel('a', Symbol('b'));
-      assert.strictEqual(spanData.span.labels.a, 'Symbol(b)');
-    });
-  });
-
-  it('truncates large span names to limit', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData(Array(200).join('a'), 1, 2);
-      assert.strictEqual(
-        spanData.span.name,
-        Array(Constants.TRACE_SERVICE_SPAN_NAME_LIMIT - 2).join('a') + '...');
-    });
-  });
-
-  it('truncates large label keys to limit', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      var longLabelKey = Array(200).join('a');
-      spanData.addLabel(longLabelKey, 5);
-      assert.strictEqual(
-        spanData.span.labels[Array(Constants.TRACE_SERVICE_LABEL_KEY_LIMIT - 2).join('a') + '...'],
-        '5');
-    });
-  });
-
-  it('truncates large label values to limit', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      var longLabelVal = Array(16550).join('a');
-      spanData.addLabel('a', longLabelVal);
-      assert.strictEqual(spanData.span.labels.a,
-        Array(traceWriter.get().getConfig().maximumLabelValueSize - 2).join('a') +
-          '...');
-    });
-  });
-
-  // TODO Add addLabel test
-
-  it('closes when endSpan is called', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('name', 1, 2);
-      assert.ok(!spanData.span.endTime);
+    it('accurately records timestamps', () => {
+      // Create another span, to determine start time correctness
+      const startLowerBound = Date.now();
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      const startUpperBound = Date.now();
+      const endLowerBound = Date.now();
       spanData.endSpan();
+      const endUpperBound = Date.now();
+      const actualStart = new Date(spanData.span.startTime).getTime();
+      const actualEnd = new Date(spanData.span.endTime).getTime();
+      const expectedTimes = [
+        startLowerBound, actualStart, startUpperBound, endLowerBound, actualEnd,
+        endUpperBound
+      ];
+      assert.ok(spanData.span.startTime);
       assert.ok(spanData.span.endTime);
+      assert.deepStrictEqual(expectedTimes.map(x => x).sort(), expectedTimes);
     });
-  });
 
-  it('captures stack traces', function() {
-    traceWriter.get().getConfig().stackTraceLimit = 25;
-    cls.get().runWithNewContext(function awesome() {
-      var spanData = createRootSpanData('name', 1, 2, 1);
-      assert.ok(!spanData.span.endTime);
-      spanData.endSpan();
-      var stack = spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
-      assert.ok(stack);
-      assert.ok(typeof stack === 'string');
-      var frames = JSON.parse(stack);
-      assert.ok(frames && frames.stack_frame);
-      assert.ok(Array.isArray(frames.stack_frame));
-      assert.strictEqual(frames.stack_frame[0].method_name, 'awesome');
+    it('truncates large span names to limit', () => {
+      const name = new Array(200).fill('a').join('');
+      const spanData = new CommonSpanData(trace, name, '0', 0);
+      assert.strictEqual(
+          spanData.span.name,
+          `${name.slice(0, Constants.TRACE_SERVICE_SPAN_NAME_LIMIT - 3)}...`);
     });
-  });
 
-  it('does not limit stack trace', function() {
-    traceWriter.get().getConfig().maximumLabelValueSize = 10;
-    cls.get().runWithNewContext(function awesome() {
-      var spanData = createRootSpanData('name', 1, 2, 1);
-      spanData.endSpan();
-      var stack = spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
-      assert.ok(stack.length > 10);
-      var frames = JSON.parse(stack);
-      assert.strictEqual(frames.stack_frame[0].method_name, 'awesome');
+    it('adds labels of different types', () => {
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      spanData.addLabel('key', 'value');
+      spanData.addLabel('id', 42);
+      spanData.addLabel('obj', {a: true});
+      spanData.addLabel('sym', Symbol('a'));
+      delete spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
+      assert.deepStrictEqual(
+          spanData.span.labels,
+          {id: '42', key: 'value', obj: '{ a: true }', sym: 'Symbol(a)'});
     });
-  });
 
-  it('should close nested spans', function() {
-    cls.get().runWithNewContext(function() {
-      var spanData = createRootSpanData('hi');
-      // create a child span
-      new ChildSpanData(spanData.trace, 'child', spanData.span.spanId, 0);
-      spanData.endSpan();
-      var traces = common.getTraces();
-      for (var i = 0; i < traces.length; i++) {
-        for (var j = 0; j < traces[i].spans.length; j++) {
-          assert.notEqual(traces[i].spans[j].endTime, '');
-        }
+    it('truncates long keys', () => {
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      const longKey = new Array(200).fill('a').join('');
+      spanData.addLabel(longKey, 'val');
+      delete spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
+      assert.deepStrictEqual(spanData.span.labels, {
+        [`${longKey.slice(0, Constants.TRACE_SERVICE_LABEL_KEY_LIMIT - 3)}...`]:
+            'val'
+      });
+    });
+
+    it('truncates long labels', () => {
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      const longVal = 'value-value-value';
+      spanData.addLabel('longKey', longVal);
+      delete spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
+      assert.deepStrictEqual(spanData.span.labels, {
+        longKey: `${
+            longVal.slice(
+                0, traceWriter.get().getConfig().maximumLabelValueSize - 3)}...`
+      });
+    });
+
+    it('exposes a method to provide serialized trace context', () => {
+      const spanData = new CommonSpanData(trace, 'name', '0', 0);
+      assert.deepStrictEqual(
+          spanData.getTraceContext(),
+          `${spanData.trace.traceId}/${spanData.span.spanId};o=1`);
+    });
+
+    it('captures stack traces', () => {
+      function myFunction() {
+        const spanData = new CommonSpanData(trace, 'name', '0', 0);
+        const stack = spanData.span.labels[TraceLabels.STACK_TRACE_DETAILS_KEY];
+        assert.ok(stack);
+        const frames = JSON.parse(stack);
+        assert.ok(frames && frames.stack_frame);
+        assert.ok(Array.isArray(frames.stack_frame));
+        // Check stack size
+        assert.strictEqual(
+            frames.stack_frame.length,
+            traceWriter.get().getConfig().stackTraceLimit);
+        // Check top frame
+        assert.strictEqual(frames.stack_frame[0].method_name, 'myFunction');
       }
+      myFunction();
+    });
+  });
+
+  describe('RootSpanData', () => {
+    it('creates child spans', () => {
+      const rootSpanData = new RootSpanData(trace, 'root', '0', 0);
+      const childSpanData =
+          rootSpanData.createChildSpan({name: 'child'}) as ChildSpanData;
+      assert.strictEqual(
+          childSpanData.span.parentSpanId, rootSpanData.span.spanId);
+    });
+
+    it('writes to a Trace Writer when ended', () => {
+      const rootSpanData = new RootSpanData(trace, 'root', '0', 0);
+      rootSpanData.endSpan();
+      assert.strictEqual(capturedTrace, rootSpanData.trace);
     });
   });
 });
-
-export default {};
