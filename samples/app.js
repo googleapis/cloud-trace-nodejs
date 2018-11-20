73786ebf9bfb03589e7ec0b438288eb72618cf9b
praveenqlogic <44371467+praveenqlogic@users.noreply.github.com>
2018-11-20 22:04:36 +0530

    docs(samples): updated samples code to use async await (#924)

--- samples/app.js
+++ samples/app.js
@@ -29,23 +29,22 @@ const app = express();
 const DISCOVERY_URL = 'https://www.googleapis.com/discovery/v1/apis';
 
 // This incoming HTTP request should be captured by Trace
-app.get('/', (req, res) => {
-  // This outgoing HTTP request should be captured by Trace
-  got(DISCOVERY_URL, { json: true })
-    .then((response) => {
-      const names = response.body.items.map((item) => item.name);
-
-      res
-        .status(200)
-        .send(names.join('\n'))
-        .end();
-    })
-    .catch((err) => {
-      console.error(err);
-      res
-        .status(500)
-        .end();
-    });
+app.get('/', async (req, res) => {
+  // This outgoing HTTP request should be captured by Trace  
+  try {
+    const { body } = await got(DISCOVERY_URL, { json: true });
+    const names = body.items.map((item) => item.name);
+    res
+      .status(200)
+      .send(names.join('\n'))
+      .end();
+  }
+  catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .end();
+  }
 });
 
 // Start the server
