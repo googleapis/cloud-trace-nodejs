12a2f8432acfc331b711d8916a1a30c471630d88
Justin Beckwith <justin.beckwith@gmail.com>
2019-11-25 08:54:39 -0800

    chore: update license headers

--- samples/app.js
+++ samples/app.js
@@ -1,17 +1,16 @@
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 
@@ -30,7 +29,7 @@ const DISCOVERY_URL = 'https://www.googleapis.com/discovery/v1/apis';
 
 // This incoming HTTP request should be captured by Trace
 app.get('/', async (req, res) => {
-  // This outgoing HTTP request should be captured by Trace  
+  // This outgoing HTTP request should be captured by Trace
   try {
     const { body } = await got(DISCOVERY_URL, { json: true });
     const names = body.items.map((item) => item.name);
--- test/plugins/test-trace-grpc.ts
+++ test/plugins/test-trace-grpc.ts
@@ -1,18 +1,16 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2016 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 'use strict';
 
 import { cls } from '../../src/cls';
@@ -336,7 +334,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -357,7 +355,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -381,7 +379,7 @@ describeInterop('grpc', fixture => {
         assert.strictEqual(clientTrace.labels.status,
             '{"code":0,"details":"OK","metadata":{"_internal_repr":{},"flags":0}}');
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -404,7 +402,7 @@ describeInterop('grpc', fixture => {
         assert.strictEqual(clientTrace.labels.status,
             '{"code":0,"details":"OK","metadata":{"_internal_repr":{},"flags":0}}');
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -427,7 +425,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert.ok(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -603,7 +601,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -622,7 +620,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -644,7 +642,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
@@ -666,7 +664,7 @@ describeInterop('grpc', fixture => {
         };
         assertTraceProperties(grpcClientPredicate);
         assertTraceProperties(grpcServerOuterPredicate);
-        // Check that a child span was created in gRPC root span 
+        // Check that a child span was created in gRPC root span
         assert(common.getMatchingSpan(grpcServerInnerPredicate));
         done();
       });
