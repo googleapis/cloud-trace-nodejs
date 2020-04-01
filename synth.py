11ea0710c8c720cefcf53971c24dd317503af59a
yoshi-automation <yoshi-automation@google.com>
2020-04-01 04:17:11 -0700

    docs: document version support goals (#1222)
    https://github.com/googleapis/cloud-trace-nodejs/commit/644adaa35efe266833a583d4375d4e82cc9d3861
    commit 644adaa35efe266833a583d4375d4e82cc9d3861
    Author: Benjamin E. Coe <bencoe@google.com>
    Date:   Mon Mar 30 11:40:16 2020 -0700
    
        docs: document version support goals (#1222)
    
        Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

--- synth.py
+++ synth.py
@@ -17,13 +17,10 @@ import synthtool.gcp as gcp
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
 # Don't use .nycrc for code coverage (see "Fix Code Coverage")
-s.copy(templates, excludes=['.nycrc', '.github/workflows/ci.yaml'])
+s.copy(templates, excludes=['.nycrc'])
 
 ### SUPPORT DATABASE PLUGINS ###
 # Database plugins require that an instance of that database is running at a
