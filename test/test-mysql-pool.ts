7b113fed7db8a8de71bade5a2c4cc47d9ce8fea5
Justin Beckwith <justin.beckwith@gmail.com>
2020-11-19 11:07:01 -0800

    boop

--- test/test-mysql-pool.ts
+++ test/test-mysql-pool.ts
@@ -31,7 +31,7 @@ if (semver.satisfies(process.version, '>=4')) {
         samplingRate: 0,
         enhancedDatabaseReporting: true,
       });
-      Hapi = require('./plugins/fixtures/hapi16');
+      Hapi = require('./plugins/fixtures/hapi18');
     });
 
     it('should work with connection pool access', done => {
