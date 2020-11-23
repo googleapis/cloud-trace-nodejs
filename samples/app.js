01ee7f9ce62093e1031a1aa353d79e378ec1e9d0
Bogdan Chadkin <trysound@yandex.ru>
2020-11-24 02:25:58 +0300

    Suppress eslint errors

--- samples/app.js
+++ samples/app.js
@@ -17,6 +17,7 @@
 // [START trace_setup_nodejs_app]
 if (process.env.NODE_ENV === 'production') {
   // [START trace_setup_nodejs_implicit]
+  // eslint-disable-next-line node/no-missing-require
   require('@google-cloud/trace-agent').start();
   // [END trace_setup_nodejs_implicit]
 }
