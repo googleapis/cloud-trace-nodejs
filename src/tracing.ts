44ce85dc4d86b46c9877737c060f20a40d62c801
Sofia Leon <sofialeon@google.com>
2022-08-16 11:21:23 -0700

    disable warning for this line

--- src/tracing.ts
+++ src/tracing.ts
@@ -170,6 +170,7 @@ export class Tracing implements Component {
     }
 
     // Make trace agent available globally without requiring package
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (global as any)._google_trace_agent = this.traceAgent;
 
     this.logger.info('StackdriverTracer#start: Trace Agent activated.');
